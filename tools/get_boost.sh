f069a6633e9a77565d0c98559e6ce5733123edb5
Damian Jarek <damian.jarek93@gmail.com>
2018-10-12 21:37:44 +0200

    Remove caching of boost dir
    The build time was almost the same and caching added unnecessary complexity.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- tools/get_boost.sh
+++ tools/get_boost.sh
@@ -7,53 +7,41 @@ if [ $1 == "master" ]; then
     branch="develop"
 fi
 
-pull_new() {
-    git clone -b $branch --depth 1 https://github.com/boostorg/boost.git boost-root
-    cd boost-root
+git clone -b $branch --depth 1 https://github.com/boostorg/boost.git boost-root
 
-    git submodule update --init libs/array
-    git submodule update --init libs/asio
-    git submodule update --init libs/assert
-    git submodule update --init libs/chrono
-    git submodule update --init libs/concept_check
-    git submodule update --init libs/config
-    git submodule update --init libs/container
-    git submodule update --init libs/container_hash
-    git submodule update --init libs/core
-    git submodule update --init libs/date_time
-    git submodule update --init libs/detail
-    git submodule update --init libs/integer
-    git submodule update --init libs/io
-    git submodule update --init libs/iterator
-    git submodule update --init libs/lexical_cast
-    git submodule update --init libs/math
-    git submodule update --init libs/move
-    git submodule update --init libs/mpl
-    git submodule update --init libs/numeric
-    git submodule update --init libs/predef
-    git submodule update --init libs/preprocessor
-    git submodule update --init libs/range
-    git submodule update --init libs/smart_ptr
-    git submodule update --init libs/static_assert
-    git submodule update --init libs/system
-    git submodule update --init libs/throw_exception
-    git submodule update --init libs/tokenizer
-    git submodule update --init libs/type_traits
-    git submodule update --init libs/utility
-    git submodule update --init libs/winapi
-    git submodule update --init tools/build
-
-    echo Submodule update complete
-    return 0
-}
-
-update_cached() {
-    cd boost-root || return $?
-    echo Updating submodules
-    return  $(git submodule update --recursive --remote)
-}
-
-update_cached || pull_new || exit 1
+cd boost-root
+git submodule update --init libs/array
+git submodule update --init libs/asio
+git submodule update --init libs/assert
+git submodule update --init libs/chrono
+git submodule update --init libs/concept_check
+git submodule update --init libs/config
+git submodule update --init libs/container
+git submodule update --init libs/container_hash
+git submodule update --init libs/core
+git submodule update --init libs/date_time
+git submodule update --init libs/detail
+git submodule update --init libs/integer
+git submodule update --init libs/io
+git submodule update --init libs/iterator
+git submodule update --init libs/lexical_cast
+git submodule update --init libs/math
+git submodule update --init libs/move
+git submodule update --init libs/mpl
+git submodule update --init libs/numeric
+git submodule update --init libs/predef
+git submodule update --init libs/preprocessor
+git submodule update --init libs/range
+git submodule update --init libs/smart_ptr
+git submodule update --init libs/static_assert
+git submodule update --init libs/system
+git submodule update --init libs/throw_exception
+git submodule update --init libs/tokenizer
+git submodule update --init libs/type_traits
+git submodule update --init libs/utility
+git submodule update --init libs/winapi
+git submodule update --init tools/build
+echo Submodule update complete
 
 rm -rf libs/certify
 mkdir libs/certify
