342bd72675d42f05c03e32fd68469b72a505ee3c
Damian Jarek <damian.jarek93@gmail.com>
2019-02-19 19:11:22 +0100

    Add documentation
    Documentation will now be built and pushed to the gh-pages branch.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

---
+++ tools/build_docs.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  return 0
+fi
+
+echo "Generting documentation"
+./b2 libs/certify/doc
+
+cd libs/certify/
+git checkout gh-pages
+cp doc/html/certify.html ~/index.html
+
+echo "Pushing documentation"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git commit -m"Documentation from $TRAVIS_COMMIT"
+
+git push https://$GH_TOKEN@github.com/djarek/certify.git > /dev/null 2>&1
