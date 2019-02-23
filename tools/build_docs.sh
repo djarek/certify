3b3e3b212a4b32ae2f928c9b6fac2604028eb258
Damian Jarek <damian.jarek93@gmail.com>
2019-02-23 04:14:23 +0100

    Fix doc build script
    The doc build script will now build the documentation if BUILD_DOCS
    is set to true.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

--- tools/build_docs.sh
+++ tools/build_docs.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$BUILD_DOCS" != "true" ]; then
   return 0
 fi
 
@@ -9,12 +9,13 @@ echo "Generting documentation"
 
 cd libs/certify/
 git checkout gh-pages
-cp doc/html/certify.html ~/index.html
+cp doc/html/certify.html index.html
 
 echo "Pushing documentation"
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config --global push.default current
+git add index.html
 git commit -m"Documentation from $TRAVIS_COMMIT"
 
 git push https://$GH_TOKEN@github.com/djarek/certify.git > /dev/null 2>&1
