5ee348efb5b4e41a309874447b8bc2848189b7c3
Damian Jarek <damian.jarek93@gmail.com>
2019-04-09 00:38:56 +0200

    Certificate status cache (#48)
    A thread-safe cache for use by the certificate verification algorithm.
    
    Signed-off-by: Damian Jarek <damian.jarek93@gmail.com>

---
+++ tests/status_cache.cpp
@@ -0,0 +1,43 @@
+#include <boost/certify/detail/status_cache.hpp>
+
+#include <boost/core/lightweight_test.hpp>
+#include <thread>
+
+int
+main()
+{
+    namespace bc = boost::certify;
+    namespace sc = std::chrono;
+
+    {
+        // Empty cache returns status unknown
+        bc::status_cache cache;
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::unknown);
+    }
+
+    {
+        bc::status_cache cache;
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::unknown);
+        cache.mark_valid("AAA", sc::system_clock::now() + sc::seconds{120});
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::valid);
+        cache.revoke("AAA");
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::revoked);
+
+        // If we ever see a revocation, mark_valid must not update
+        cache.mark_valid("AAA", sc::system_clock::now() + sc::seconds{120});
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::revoked);
+    }
+
+    {
+        bc::status_cache cache;
+        cache.mark_valid("AAA", sc::system_clock::now() + sc::microseconds{10});
+        std::this_thread::sleep_for(std::chrono::microseconds{11});
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::unknown);
+
+        // Stale validity check must not update the status
+        cache.mark_valid("AAA", sc::system_clock::now() - sc::microseconds{10});
+        BOOST_TEST(cache.check("AAA") == bc::certificate_status::unknown);
+    }
+
+    return boost::report_errors();
+}
